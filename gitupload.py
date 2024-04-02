f808a83f6dc9752fdb5135dae27ab13ad6e72f56
jurikri <jurikri@google.com>
2024-04-02 14:39:07 +0900

    1

--- gitupload.py
+++ gitupload.py
@@ -20,3 +20,18 @@ subprocess.run(['git', 'commit', '-m', commit_message], check=True)
 
 # 변경사항 원격 저장소에 푸시
 subprocess.run(['git', 'push'], check=True)
+
+#%%
+import subprocess
+import os
+
+# 경로 변경 (프로젝트 폴더로 이동)
+os.chdir('C:/mscode/test')
+
+# 최신 변경사항 다운로드 및 업데이트
+try:
+    result = subprocess.run(['git', 'pull'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print("업데이트 성공:\n", result.stdout.decode())
+except subprocess.CalledProcessError as e:
+    print("업데이트 실패:\n", e.stderr.decode())
+
